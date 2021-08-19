1b1bf598b251b63ef6c456a79043546b1ce2a620
kikejosan <enriquebs96@gmail.com>
2021-08-19 19:43:40 +0200

    Replace read_parquet with h1 changed

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ df = pd.read_parquet('/tmp/social_network.parquet')
 
 # LAYOUT
 app.layout = html.Div([
-    html.H1('Dashboard Social Networks', style={
+    html.H1('Dashboard Social Networks - First CD commit', style={
             "text-align": "center", "margin-top": "24px", "margin-bottom": "48px"}),
     html.Div([
         html.Label('Datetime Range'),
